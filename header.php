62d9da7f3a576edf7f1ff85f9d00867d9be13c75
achraf guemati <guematiachraf@isamm.u-manouba.tn>
2025-10-21 22:02:16 +0100

    Fix: Add Revolution Slider JS files and fallback handler

--- header.php
+++ header.php
@@ -25,6 +25,18 @@
     <link href="assets/css/style-dark.css" rel="stylesheet" disabled>
     <!-- Load jQuery first -->
     <script src="assets/service/js/jquery.min.js"></script>
+    <!-- Load Revolution Slider JS files -->
+    <script src="assets/revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script src="assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
     <!--[if lt IE 9]>
       <script src="assets/js/html5shiv.js"></script>
       <script src="assets/js/respond.min.js"></script>
